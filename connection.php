86b3227fb9d5d40170839bbaae74d6d7f0743c1d
Favour58 <110856204+Favour58@users.noreply.github.com>
2023-01-27 11:07:33 -0800

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+$con  = mysqli_connect('localhost','root','','php_login');
+
+if(!$con)
+{
+    echo 'please check database connection';
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- <link rel="stylesheet" href="style.css"> -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+            <div class="row">
+                <div class="col-lg-6 m-auto">
+                    <div class="card mt-4">
+                        <div class="card-title">
+                            <h2 class="text-center py-4"> Signup Form </h2>
+                            <hr>
+                  <div class="signup">
+                      <form action="process.php" method="post">
+                   <input type="text" placeholder="UserName" class="form-control mb-2 py-2" name="UserName"><br>
+                     <input type="email" placeholder="Email" class="form-control mb-2 py-2" name="Email"><br>
+                    <input type="password" placeholder="Password" class="form-control mb-2 py-2" name="Password"><br>
+                    <input type="password" placeholder="Confirm Password" class="form-control mb-2 py-2" name="Cpass"><br><br><br>
+                     <input type="submit" value="Create An Account" class="btn btn-success float-right" name="btn-save">
+                     </form>
+    </div>
+       </div>
+         </div>
+           </div>
+              </div>
+    
+</body>
+</html>
+
+
+
+
+
+
+
+ <!-- <input type="text" placeholder="UserName" class="txt" name="UserName"><br>
+            <input type="email" placeholder="Email" class="txt1" name="Email"><br>
+            <input type="password" placeholder="Password" class="txt2" name="Password"><br>
+            <input type="password" placeholder="Confirm Password" class="txt3" name="Cpass"><br><br><br>
+            <input type="submit" value="Create An Account" class="btn" name="btn-save"> -->
